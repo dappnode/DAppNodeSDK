f0ccba6402aafed024a37a9d8359c5d763dd383f
dapplion <guillem.lion.benosa@gmail.com>
2020-10-19 11:41:20 +0200

    Expose init command programatically

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -66,138 +66,151 @@ export const init: CommandModule<CliGlobalOptions, CliCommandOptions> = {
         type: "boolean"
       }),
 
-  handler: async ({ yes: useDefaults, force, dir }): Promise<void> => {
-    // shell outputs tend to include trailing spaces and new lines
-    const directoryName = await shell('echo "${PWD##*/}"');
-    const defaultAuthor = await shell("whoami");
-    const defaultName = getDnpName(directoryName);
-
-    const defaultAnswers = {
-      name: defaultName,
-      version: defaultVersion,
-      description: `${defaultName} description`,
-      avatar: "",
-      type: "service",
-      author: defaultAuthor,
-      license: "GLP-3.0"
-    };
-
-    if (!useDefaults) {
-      console.log(`This utility will walk you through creating a dappnode_package.json file.
-It only covers the most common items, and tries to guess sensible defaults.
-`);
-    }
-
-    if (fs.existsSync(path.join(dir, manifestPath)) && !force) {
-      const continueAnswer = await inquirer.prompt([
-        {
-          type: "confirm",
-          name: "continue",
-          message:
-            "This directory is already initialized. Are you sure you want to overwrite the existing manifest?"
-        }
-      ]);
-      if (!continueAnswer.continue) {
-        throw new YargsError("Stopping");
-      }
-    }
-
-    const answers = useDefaults
-      ? defaultAnswers
-      : await inquirer.prompt([
-          {
-            type: "input",
-            name: "name",
-            default: defaultAnswers.name,
-            message: "DAppNodePackage name"
-          },
-          {
-            type: "input",
-            name: "version",
-            default: defaultAnswers.version,
-            message: "Version",
-            validate: val =>
-              !semver.valid(val) ||
-              !(
-                semver.eq(val, "1.0.0") ||
-                semver.eq(val, "0.1.0") ||
-                semver.eq(val, "0.0.1")
-              )
-                ? "the version needs to be valid semver. If this is the first release, the version must be 1.0.0, 0.1.0 or 0.0.1 "
-                : true
-          },
-          {
-            type: "input",
-            name: "description",
-            message: "Description",
-            default: defaultAnswers.description
-          },
-          {
-            type: "input",
-            message: "Author",
-            name: "author",
-            default: defaultAnswers.author
-          },
-          {
-            type: "input",
-            message: "License",
-            name: "license",
-            default: defaultAnswers.license
-          }
-        ]);
-
-    // Construct DNP
-    const manifest: Manifest = {
-      name: answers.name ? getDnpName(answers.name) : defaultName,
-      version: answers.version || defaultVersion,
-      description: answers.description,
-      type: "service",
-      author: answers.author,
-      categories: ["Developer tools"],
-      links: {
-        homepage: "https://your-project-homepage-or-docs.io"
-      },
-      license: answers.license
-    };
-
-    // Create folders
-    await shell(`mkdir -p ${path.join(dir, "build")}`);
-
-    // Write manifest and compose
-    writeManifest(dir, manifest);
-    generateAndWriteCompose(dir, manifest);
-
-    // Add default avatar so users can run the command right away
-    const files = fs.readdirSync(dir);
-    const avatarFile = files.find(file => releaseFiles.avatar.regex.test(file));
-    if (!avatarFile) {
-      fs.writeFileSync(
-        path.join(dir, avatarPath),
-        Buffer.from(avatarData, "base64")
-      );
-    }
-
-    // Initialize Dockerfile
-    fs.writeFileSync(path.join(dir, dockerfilePath), dockerfileData);
-
-    // Initialize .gitignore
-    writeGitIgnore(path.join(dir, gitignorePath));
+  handler: async args => {
+    const manifest = await initHandler(args);
 
     console.log(`
-      ${chalk.green("Your DAppNodePackage is ready")}: ${manifest.name}
+    ${chalk.green("Your DAppNodePackage is ready")}: ${manifest.name}
 
 To start, you can:
 
- - Develop your dockerized app in   ./build
- - Add settings in the compose at   ./docker-compose.yml
- - Add metadata in the manifest at  ./dappnode_package.json
+- Develop your dockerized app in   ./build
+- Add settings in the compose at   ./docker-compose.yml
+- Add metadata in the manifest at  ./dappnode_package.json
 
 Once ready, you can build, install, and test it by running
 
-  dappnodesdk build 
+dappnodesdk build 
 `);
   }
 };
+
+/**
+ * Common handler for CLI and programatic usage
+ */
+export async function initHandler({
+  yes: useDefaults,
+  force,
+  dir
+}: CliCommandOptions): Promise<Manifest> {
+  // shell outputs tend to include trailing spaces and new lines
+  const directoryName = await shell('echo "${PWD##*/}"');
+  const defaultAuthor = await shell("whoami");
+  const defaultName = getDnpName(directoryName);
+
+  const defaultAnswers = {
+    name: defaultName,
+    version: defaultVersion,
+    description: `${defaultName} description`,
+    avatar: "",
+    type: "service",
+    author: defaultAuthor,
+    license: "GLP-3.0"
+  };
+
+  if (!useDefaults) {
+    console.log(`This utility will walk you through creating a dappnode_package.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+`);
+  }
+
+  if (fs.existsSync(path.join(dir, manifestPath)) && !force) {
+    const continueAnswer = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "continue",
+        message:
+          "This directory is already initialized. Are you sure you want to overwrite the existing manifest?"
+      }
+    ]);
+    if (!continueAnswer.continue) {
+      throw new YargsError("Stopping");
+    }
+  }
+
+  const answers = useDefaults
+    ? defaultAnswers
+    : await inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          default: defaultAnswers.name,
+          message: "DAppNodePackage name"
+        },
+        {
+          type: "input",
+          name: "version",
+          default: defaultAnswers.version,
+          message: "Version",
+          validate: val =>
+            !semver.valid(val) ||
+            !(
+              semver.eq(val, "1.0.0") ||
+              semver.eq(val, "0.1.0") ||
+              semver.eq(val, "0.0.1")
+            )
+              ? "the version needs to be valid semver. If this is the first release, the version must be 1.0.0, 0.1.0 or 0.0.1 "
+              : true
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Description",
+          default: defaultAnswers.description
+        },
+        {
+          type: "input",
+          message: "Author",
+          name: "author",
+          default: defaultAnswers.author
+        },
+        {
+          type: "input",
+          message: "License",
+          name: "license",
+          default: defaultAnswers.license
+        }
+      ]);
+
+  // Construct DNP
+  const manifest: Manifest = {
+    name: answers.name ? getDnpName(answers.name) : defaultName,
+    version: answers.version || defaultVersion,
+    description: answers.description,
+    type: "service",
+    author: answers.author,
+    categories: ["Developer tools"],
+    links: {
+      homepage: "https://your-project-homepage-or-docs.io"
+    },
+    license: answers.license
+  };
+
+  // Create folders
+  await shell(`mkdir -p ${path.join(dir, "build")}`);
+
+  // Write manifest and compose
+  writeManifest(dir, manifest);
+  generateAndWriteCompose(dir, manifest);
+
+  // Add default avatar so users can run the command right away
+  const files = fs.readdirSync(dir);
+  const avatarFile = files.find(file => releaseFiles.avatar.regex.test(file));
+  if (!avatarFile) {
+    fs.writeFileSync(
+      path.join(dir, avatarPath),
+      Buffer.from(avatarData, "base64")
+    );
+  }
+
+  // Initialize Dockerfile
+  fs.writeFileSync(path.join(dir, dockerfilePath), dockerfileData);
+
+  // Initialize .gitignore
+  writeGitIgnore(path.join(dir, gitignorePath));
+
+  return manifest;
+}
 
 /**
  * Parses a directory or generic package name and returns a full ENS guessed name
