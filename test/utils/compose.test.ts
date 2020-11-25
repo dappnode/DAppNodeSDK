03d21c3685cdc44db48062d3f3e0b02c67f9b341
dapplion <guillem.lion.benosa@gmail.com>
2020-11-25 12:57:46 +0000

    Parse upstreamVersion tag better for multi-service

--- test/utils/compose.test.ts
+++ test/utils/compose.test.ts
@@ -154,5 +154,33 @@ describe("util > compose", () => {
         "Prysm: v1.0.0-alpha.25, Lighthouse: v0.2.9"
       );
     });
+
+    it("Should parse the same upstream version for multi-service", () => {
+      const compose: Compose = {
+        version: "3.4",
+        services: {
+          validator: {
+            image: "sample-image",
+            build: {
+              args: {
+                UPSTREAM_VERSION: "v1.0.0"
+              }
+            }
+          },
+          "beacon-chain": {
+            image: "sample-image",
+            build: {
+              args: {
+                UPSTREAM_VERSION: "v1.0.0"
+              }
+            }
+          }
+        }
+      };
+
+      const upstreamVersion = parseComposeUpstreamVersion(compose);
+
+      expect(upstreamVersion).to.equal("v1.0.0");
+    });
   });
 });
