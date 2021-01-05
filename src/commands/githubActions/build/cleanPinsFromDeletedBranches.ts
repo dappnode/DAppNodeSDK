d690b628f107f5334bdbae99df5f803b78e4c2a6
dapplion <guillem.lion.benosa@gmail.com>
2021-01-05 20:29:39 +0100

    Clean old pins in build action

---
+++ src/commands/githubActions/build/cleanPinsFromDeletedBranches.ts
@@ -0,0 +1,44 @@
+import { fetchPinsGroupedByBranch } from "../../../pinStrategy";
+import { cliArgsToReleaseUploaderProvider } from "../../../releaseUploader";
+import { PinataPinManager } from "../../../releaseUploader/pinata";
+import { readManifest } from "../../../utils/manifest";
+import { Github } from "../../../providers/github/Github";
+
+/**
+ * Removes all pins associated with a branch that no longer exists
+ */
+export async function cleanPinsFromDeletedBranches({
+  dir
+}: {
+  dir: string;
+}): Promise<void> {
+  // Read manifest from disk to get package name
+  const manifest = readManifest(dir);
+
+  // Connect to Github Octokit REST API to know existing branches
+  const github = new Github(dir);
+  const branches = await github.listBranches();
+
+  // Fetch Pinata credentials from ENVs
+  const releaseUploaderProvider = cliArgsToReleaseUploaderProvider({
+    uploadTo: "ipfs",
+    contentProvider: "pinata"
+  });
+  if (releaseUploaderProvider.type !== "pinata")
+    throw Error("Must use pinata for deletePins");
+  const pinata = new PinataPinManager(releaseUploaderProvider);
+
+  const pinsGroupedByBranch = await fetchPinsGroupedByBranch(pinata, manifest);
+  for (const { branch, pins } of pinsGroupedByBranch) {
+    if (branches.find(b => b.name === branch)) continue;
+
+    // Branch not found, delete all associated pins
+    for (const pin of pins) {
+      console.log(`Unpin ${pin.commit} ${pin.ipfsHash}`);
+      await pinata.unpin(pin.ipfsHash).catch(e => {
+        // Don't prevent unpinning other pins if one is faulty
+        console.error(`Error on unpin ${pin.ipfsHash}`, e);
+      });
+    }
+  }
+}
--- src/pinStrategy/index.ts
+++ src/pinStrategy/index.ts
@@ -2,26 +2,15 @@ import { PinataMetadata, PinataPinManager } from "../releaseUploader/pinata";
 import { Manifest } from "../types";
 import { GitHead } from "../utils/git";
 import { prettyPinataPinName } from "../utils/format";
+import { DnpPinMetadata, PinDataSummary, PinsByBranch } from "./types";
+import { groupPinsByBranch } from "./utils";
 
-/**
- * All logic regarding the pin strategy of DAppNode packages
- * In development, builds will be uploaded on each push on a PR
- * only the latest commit of each branch will stay pinned.
- *
- * When PR is merged (and the branch is deleted) all pins associated to
- * that branch will be unpinned
- */
-
-/**
- * Pinata DAppNodePackage build pin metadata
- */
-export interface DnpPinMetadata {
-  name: string;
-  version: string;
-  upstreamVersion: string | undefined;
-  commit: string | undefined;
-  branch: string | undefined;
-}
+// This file has all logic for the pin strategy of DAppNode packages
+// In development, builds will be uploaded on each push on a PR
+// only the latest commit of each branch will stay pinned.
+//
+// When PR is merged (and the branch is deleted) all pins associated to
+// that branch will be unpinned
 
 export function getPinMetadata(
   manifest: Manifest,
@@ -30,7 +19,7 @@ export function getPinMetadata(
   return {
     name: prettyPinataPinName(manifest, gitHead),
     keyvalues: {
-      name: manifest.name,
+      dnpName: manifest.name,
       version: manifest.version,
       upstreamVersion: manifest.upstreamVersion,
       commit: gitHead ? gitHead.commit : undefined,
@@ -39,18 +28,9 @@ export function getPinMetadata(
   };
 }
 
-interface PinDataSummary {
-  ipfsHash: string;
-  commit?: string;
-}
-
 /**
  * Fetch pins with same branch, assuming pins are upload with `DnpPinMetadata` metadata.
  * Can be used to clean all pins from a deleted branch with
- * ```ts
- * const pins = await fetchPinsWithBranch(pinata, manifest, gitHead);
- * for (const pin of pins) await pinata.unpin(pin.ipfsHash);
- * ```
  */
 export async function fetchPinsWithBranch(
   pinata: PinataPinManager,
@@ -71,22 +51,31 @@ export async function fetchPinsWithBranch(
 
 /**
  * Fetch pins with same branch, assuming pins are upload with `DnpPinMetadata` metadata.
+ * Can be used to clean all pins from a deleted branch with
+ */
+export async function fetchPinsGroupedByBranch(
+  pinata: PinataPinManager,
+  manifest: Manifest
+): Promise<PinsByBranch[]> {
+  const pins = await pinata.pinList<DnpPinMetadata>({
+    keyvalues: {
+      dnpName: { value: manifest.name, op: "eq" }
+    }
+  });
+
+  return groupPinsByBranch(pins);
+}
+
+/**
+ * Fetch pins with same branch, assuming pins are upload with `DnpPinMetadata` metadata.
  * Returns only pins associated with a commit that is previous
  * to current commit from `gitHead`, according to `git --is-ancestor`.
  */
-export async function fetchOldPinsWithBranch(
+export async function fetchPinsWithBranchToDelete(
   pinata: PinataPinManager,
   manifest: Manifest,
   gitHead: GitHead
 ): Promise<PinDataSummary[]> {
   const pins = await fetchPinsWithBranch(pinata, manifest, gitHead);
-
-  const oldPinsToDelete: PinDataSummary[] = [];
-  for (const pin of pins) {
-    if (pin.commit && pin.commit !== gitHead.commit) {
-      oldPinsToDelete.push(pin);
-    }
-  }
-
-  return oldPinsToDelete;
+  return pins.filter(pin => pin.commit && pin.commit !== gitHead.commit);
 }
