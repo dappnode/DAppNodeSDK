28ca20f7e5caa4398874a3f276f34be128b2e3f5
dapplion <guillem.lion.benosa@gmail.com>
2021-01-05 20:31:14 +0100

    Remove on-delete action

--- src/commands/githubActions/index.ts
+++ src/commands/githubActions/index.ts
@@ -1,7 +1,6 @@
 import { CommandModule } from "yargs";
 import { CliGlobalOptions } from "../../types";
 import { gaBuild } from "./build";
-import { unpinOnRefDelete } from "./unpin-on-ref-delete";
 
 export const githubActions: CommandModule<
   CliGlobalOptions,
@@ -10,8 +9,7 @@ export const githubActions: CommandModule<
   command: "github-action",
   describe:
     "Github actions tooling to be run in CI. Uses a specific set of options for internal DAppNode use. Caution: options may change without notice.",
-  builder: yargs =>
-    yargs.command(gaBuild).command(unpinOnRefDelete).demandCommand(1),
+  builder: yargs => yargs.command(gaBuild).demandCommand(1),
   handler: async (): Promise<void> => {
     throw Error("Requires 1 command");
   }
