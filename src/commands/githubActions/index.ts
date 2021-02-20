499a4fe0ba7f1aaad260df83677f7f197df611d7
dapplion <35266934+dapplion@users.noreply.github.com>
2021-02-20 15:00:31 +0100

    Register new bump-upstream command

--- src/commands/githubActions/index.ts
+++ src/commands/githubActions/index.ts
@@ -1,6 +1,7 @@
 import { CommandModule } from "yargs";
 import { CliGlobalOptions } from "../../types";
 import { gaBuild } from "./build";
+import { gaBumpUpstream } from "./bumpUpstream";
 
 export const githubActions: CommandModule<
   CliGlobalOptions,
@@ -9,7 +10,8 @@ export const githubActions: CommandModule<
   command: "github-action",
   describe:
     "Github actions tooling to be run in CI. Uses a specific set of options for internal DAppNode use. Caution: options may change without notice.",
-  builder: yargs => yargs.command(gaBuild).demandCommand(1),
+  builder: yargs =>
+    yargs.command(gaBuild).command(gaBumpUpstream).demandCommand(1),
   handler: async (): Promise<void> => {
     throw Error("Requires 1 command");
   }
