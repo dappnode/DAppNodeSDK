a1ae9b078178f4c399821b2da1c94b2333a9d798
Tropicar <zerozept@protonmail.com>
2021-04-26 12:27:53 +0200

    Use a node library to find out the os
    Change the way of finding out the architecture, instead of uname command, using the node module os. os.arch()

--- src/utils/getArchitecture.ts
+++ src/utils/getArchitecture.ts
@@ -1,17 +1,17 @@
 import { Architecture } from "../types";
 import { shell } from "./shell";
+import os from "os";
 
 /**
  * Return what kind of architecture is using the machine where the build is done
  */
 export async function getArchitecture(): Promise<Architecture> {
-  const systemInfo = await shell(`uname -a`);
-  // we will check if the machine uses arm, arm architecture usually shows something like this
-  //const systemInfo = `Linux raspberry 4.4.48-v7+ #964 SMP Mon Feb 13 16:57:51 GMT 2017 armv7l GNU/Linux`;
+  const arch = os.arch(); // Returns the operating system CPU architecture for which the Node.js binary was compiled.
 
-  const match = systemInfo.match(/(arm)|(ARM)/g);
+  //check if the machine uses arm64
   let res: Architecture;
-  if (match) {
+
+  if (arch === "arm64") {
     res = `linux/arm64`;
   } else {
     res = `linux/amd64`;
