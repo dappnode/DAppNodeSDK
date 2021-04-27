c31df4feedca0a4b668b379bacbbeab0edc9ef04
Tropicar <zerozept@protonmail.com>
2021-04-27 14:38:36 +0200

    Changing the async function to sync

--- src/utils/getArchitecture.ts
+++ src/utils/getArchitecture.ts
@@ -4,7 +4,7 @@ import os from "os";
 /**
  * Return what kind of architecture is using the machine where the build is done
  */
-export async function getArchitecture(): Promise<Architecture> {
+export function getArchitecture(): Architecture {
   const arch = os.arch(); // Returns the operating system CPU architecture for which the Node.js binary was compiled.
 
   //check if the machine uses arm64
