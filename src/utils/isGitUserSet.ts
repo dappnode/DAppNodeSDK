a301f39e46719c6969da0bc1f899b02c5e43811c
Tropicar <zerozept@protonmail.com>
2021-08-17 10:22:29 +0200

    fix isgituserset function

--- src/utils/isGitUserSet.ts
+++ src/utils/isGitUserSet.ts
@@ -1,18 +1,15 @@
 import { shell } from "./shell";
 
+/**
+ *
+ * @returns true if git user.name is defined in the machine
+ */
+
 export async function isGitUserSet(): Promise<boolean> {
   try {
     const currentUser = await shell(`git config user.name`);
-
-    // we need to check it out if thee value exists
-    if (currentUser.length > 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return Boolean(currentUser);
   } catch (e) {
-    // Empty value or not value defined returns shell error, we catch that error and return false
-    console.log("here");
     return false;
   }
 }
