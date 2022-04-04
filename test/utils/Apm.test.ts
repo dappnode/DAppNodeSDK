b1fd61a1c0f1166c35ac6f20f6cf0e353e41416a
dapplion <35266934+dapplion@users.noreply.github.com>
2022-04-04 19:11:16 +0200

    Remove if() from tests

--- test/utils/Apm.test.ts
+++ test/utils/Apm.test.ts
@@ -10,30 +10,29 @@ describe("Apm constructor", function () {
   it("Should get the contract the registry contract of a DNP name", async () => {
     const apm = new Apm("infura");
     const registry = await apm.getRegistryContract(dnpName);
-    expect(registry).to.be.ok;
-    if (registry) {
-      expect(registry.address).to.be.a("string", "Contract instance changed");
-      expect(registry.address.toLowerCase()).to.equal(
-        "0x266BFdb2124A68beB6769dC887BD655f78778923".toLowerCase()
-      );
-    }
+
+    if (!registry) throw Error("no registry");
+    expect(registry.address).to.be.a("string", "Contract instance changed");
+    expect(registry.address.toLowerCase()).to.equal(
+      "0x266BFdb2124A68beB6769dC887BD655f78778923".toLowerCase()
+    );
   });
 
   it("Should get the contract the repo contract of a DNP name", async () => {
     const apm = new Apm("infura");
     const repo = await apm.getRepoContract(dnpName);
-    expect(repo).to.be.ok;
-    if (repo) {
-      expect(repo.address).to.be.a("string", "Contract instance changed");
-      expect(repo.address.toLowerCase()).to.equal(
-        "0xEe66C4765696C922078e8670aA9E6d4F6fFcc455".toLowerCase()
-      );
-    }
+
+    if (!repo) throw Error("no repo");
+    expect(repo.address).to.be.a("string", "Contract instance changed");
+    expect(repo.address.toLowerCase()).to.equal(
+      "0xEe66C4765696C922078e8670aA9E6d4F6fFcc455".toLowerCase()
+    );
   });
 
   it("Should get the latest of a DNP name", async () => {
     const apm = new Apm("infura");
     const latestVersion = await apm.getLatestVersion(dnpName);
+
     expect(latestVersion).to.be.a("string", "Contract instance changed");
     expect(Boolean(semver.valid(latestVersion))).to.equal(
       true,
