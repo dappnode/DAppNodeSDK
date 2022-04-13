6803e78ca61c9cd056af88980a3308b885815f1c
pablo <mendez4a@gmail.com>
2022-04-13 12:29:12 +0200

    Set manifest package name pattern. Removed deprecated checks
    simplify compose code

--- src/validation/manifest/validateManifest.ts
+++ src/validation/manifest/validateManifest.ts
@@ -1,4 +1,4 @@
-import { CliError, releaseFilesDefaultNames } from "../../params";
+import { CliError } from "../../params";
 import { Manifest } from "../../types";
 import Ajv from "ajv";
 import ajvErrors from "ajv-errors";
@@ -6,24 +6,6 @@ import manifestSchema from "./manifest.schema.json";
 import { processError } from "../processError";
 
 export function validateManifest(manifest: Manifest): void {
-  if (/[A-Z]/.test(manifest.name))
-    throw new CliError("Package name in the manifest must be lowercase");
-
-  // Make sure the release is of correct type
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  if ((manifest as any).image)
-    throw new CliError(`
-DAppNode packages expect all docker related data to be contained only
-in the docker-compose.yml. Please translate the settings in 'manifest.image'
-to your package's docker-compose.yml and then delete the 'manifest.image' prop.
-`);
-  if (manifest.avatar)
-    throw new CliError(`
-DAppNode packages expect the avatar to be located at the root folder as a file
-and not declared in the manifest. Please add your package avatar to this directory
-as ${releaseFilesDefaultNames.avatar} and then remove the 'manifest.avatar' property.
-`);
-
   const { valid, errors } = validateManifestSchema(manifest);
   if (valid) return;
 
