b60609de3fbf19b5971887d80b67d714138feb38
Pablo Mendez <mendez4a@gmail.com>
2023-03-14 16:54:27 +0100

    fix test

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -12,13 +12,14 @@ export function cleanTestDir(): void {
 export function generateCompose(manifest: Manifest): Compose {
   const dnpName = manifest.name,
     serviceName = manifest.name,
+    isMonoService = true,
     version = manifest.version;
   return {
     version: "3.4",
     services: {
       [serviceName]: {
         build: ".", // Dockerfile is in root dir
-        image: getImageTag({ dnpName, serviceName, version }),
+        image: getImageTag({ dnpName, serviceName, version, isMonoService }),
         restart: "unless-stopped"
       }
     }
