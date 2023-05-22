cfdeeaac7ae9015fb247edb1c942f6cd278f2a6d
Pablo mendez <mendez4a@gmail.com>
2023-05-22 23:34:16 +0200

    remove arrayToSemver

--- test/utils/semverToArray.test.ts
+++ test/utils/semverToArray.test.ts
@@ -1,6 +1,5 @@
 import { expect } from "chai";
 import { semverToArray } from "../../src/utils/semverToArray.js";
-import { arrayToSemver } from "../../src/utils/arrayToSemver.js";
 
 describe("semver to array conversions", () => {
   const semver = "0.1.5";
@@ -8,9 +7,5 @@ describe("semver to array conversions", () => {
 
   it("should convert a semver version to array", () => {
     expect(semverToArray(semver)).to.deep.equal(versionArray);
-  });
-
-  it("should convert a version array to semver", () => {
-    expect(arrayToSemver(versionArray)).to.equal(semver);
   });
 });
