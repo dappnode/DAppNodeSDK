725b47a521c7ed08e528620cee3ca01c8070823c
Marc Font <marc.font.miralles@estudiantat.upc.edu>
2023-05-23 00:05:07 +0200

    add version test

--- test/utils/Apm.test.ts
+++ test/utils/Apm.test.ts
@@ -1,6 +1,8 @@
+import semver from "semver";
 import { expect } from "chai";
 import { getEthereumProviderUrl } from "../../src/utils/Apm.js";
 import { ethers } from "ethers";
+import { ApmRepository } from "@dappnode/toolkit";
 describe("Apm constructor", function () {
   this.timeout(60_000);
 
@@ -25,6 +27,18 @@ describe("Apm constructor", function () {
     expect(repo).to.be.a("string", "Contract instance changed");
     expect(repo.toLowerCase()).to.equal(
       "0xEe66C4765696C922078e8670aA9E6d4F6fFcc455".toLowerCase()
+    );
+  });
+
+  it("Should get the latest of a DNP name", async () => {
+    const parsedProvider = new ethers.JsonRpcProvider(getEthereumProviderUrl("infura"))
+    const apm = new ApmRepository(parsedProvider)
+    const latestVersion = await apm.getVersionAndIpfsHash({ dnpName })
+
+    expect(latestVersion.version).to.be.a("string", "Contract instance changed");
+    expect(Boolean(semver.valid(latestVersion.version))).to.equal(
+      true,
+      `Resulting version is not a valid semver: ${semver}`
     );
   });
 });
