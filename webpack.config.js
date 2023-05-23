70cff0e702a41d51682163ff8e4cf54d0bad5fad
Pablo mendez <mendez4a@gmail.com>
2023-05-23 16:18:01 +0200

    implement webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const currentModulePath = fileURLToPath(import.meta.url);
+
+export default {
+  entry: "./src/index.ts",
+  mode: "production",
+  target: "node",
+  output: {
+    path: path.resolve(path.dirname(currentModulePath), "dist"),
+    filename: "index.js",
+    library: {
+      type: "module"
+    },
+    chunkFormat: "commonjs" // specify chunkFormat as commonjs
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    extensionAlias: {
+      ".js": [".ts", ".js"],
+      ".cjs": [".cts", ".cjs"],
+      ".mjs": [".mts", ".mjs"]
+    }
+  },
+  experiments: {
+    outputModule: true // Enable the output module experiment
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            configFile: "tsconfig.json",
+            allowTsInNodeModules: true
+          }
+        }
+        // exclude: /node_modules/
+      },
+      {
+        test: /\.node$/, // add a rule for ".node" files
+        loader: "node-loader"
+      }
+    ]
+  }
+};
