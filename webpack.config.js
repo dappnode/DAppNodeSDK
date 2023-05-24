17b34d547500850615adbd6d4624b47cce8a6ba1
Pablo mendez <mendez4a@gmail.com>
2023-05-24 10:00:29 +0200

    fix sdk issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import webpack from "webpack";
 
 const currentModulePath = fileURLToPath(import.meta.url);
 
 export default {
-  entry: "./src/index.ts",
+  entry: "./src/dappnodesdk.ts",
   mode: "production",
   target: "node",
+  node: {
+    // add commonjs globals
+    __filename: true,
+    __dirname: true
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }) // this adds the removed shebang
+  ],
   output: {
     path: path.resolve(path.dirname(currentModulePath), "dist"),
     filename: "dappnodesdk.js",
     library: {
       type: "module"
     },
-    chunkFormat: "module"
+    chunkFormat: "module",
+    globalObject: "this"
   },
   resolve: {
+    alias: {
+      "any-observable": "rxjs" // Error: Cannot find any-observable implementation nor global.Observable. You must install polyfill or call require("any-observable/register") with your preferred implementation,
+    },
     extensions: [".ts", ".tsx", ".js"],
     extensionAlias: {
       ".js": [".ts", ".js"],
@@ -41,6 +54,9 @@ export default {
       },
       {
         test: /\.node$/, // add a rule for ".node" files
+        include: [
+          path.resolve(currentModulePath, "node_modules/ipfs-utils/src")
+        ],
         loader: "node-loader"
       }
     ]
