ca2163c3dc5b6698b2d98bfe91897ce2f4b70596
dappnodedev <diego@dappnode.io>
2024-03-25 10:31:17 +0100

    Squashed commit of the following:
    commit f3901de87acadf3db0242e01cae089ec79239b8e
    Author: dappnodedev <diego@dappnode.io>
    Date:   Thu Mar 21 17:08:59 2024 +0100
    
        Squashed commit of the following:
    
        commit 813907d649b61357dfc87461669bb25a254947f9
        Author: dappnodedev <144998261+dappnodedev@users.noreply.github.com>
        Date:   Thu Mar 21 16:52:54 2024 +0100
    
            Refactor bump code (#407)
    
            * Refactor bump code
    
            * Check available ETH provider on first attempt
    
            * Improve git management
    
            * Fix pre-release handling
    
            * Fix bump tests
    
            * Rename valid release test
    
            * Fix import paths
    
            * Quick fixes after PR review
    
            * Create module github
    
            * Remove SDK build envs from file
    
            * Modularize bump settings
    
            * Add comment
    
            * Fix bump tests
    
            * Add ".js" to import paths
    
    commit d50da2f7890c1fdc2dbb90a5c6508ac2737384a6
    Merge: f56a1e6 64bcc60
    Author: dappnodedev <diego@dappnode.io>
    Date:   Wed Mar 13 20:58:14 2024 +0100
    
        Merge branch 'dappnodedev/add-template-mode-to-build' into dappnodedev/enable-templating
    
    commit f56a1e64641b83a7f7a33ddf5ef86678a9e291e0
    Author: dappnodedev <144998261+dappnodedev@users.noreply.github.com>
    Date:   Tue Mar 12 14:48:10 2024 +0100
    
        Add template mode to build command (#398)
    
        * Add build args for template mode and variants
    
        * Merge manifest and compose files
    
        * Build package variants
    
        * Build all variants if none is defined
    
        * Show DNP name on build
    
        * Separate build dirs
    
        * Fix build test
    
        * Improve build handler style
    
        * Perform modifications based on review
    
        * Improve error handling in compose load
    
        * Squashed commit of the following:
    
        commit 28cae58e44fda944233293d0b70c17de946ce3f0
        Merge: dddaec8 ff912ae
        Author: dappnodedev <144998261+dappnodedev@users.noreply.github.com>
        Date:   Tue Mar 12 12:15:35 2024 +0100
    
            Merge pull request #402 from dappnode/dappnodedev/bump-js-yaml-version
    
            Bump js-yaml to 4.1.0
    
        commit ff912ae44d2c14b42f8a495990ec61cd30cb4d69
        Author: dappnodedev <diego@dappnode.io>
        Date:   Mon Mar 11 17:58:03 2024 +0100
    
            Bump js-yaml to 4.1.0
    
        * Replace template flag by variants or all-variants
    
        * Add templateMode flag to buildAndUpload
    
        * Add templateMode flag to build call
    
    commit 28cae58e44fda944233293d0b70c17de946ce3f0
    Merge: dddaec8 ff912ae
    Author: dappnodedev <144998261+dappnodedev@users.noreply.github.com>
    Date:   Tue Mar 12 12:15:35 2024 +0100
    
        Merge pull request #402 from dappnode/dappnodedev/bump-js-yaml-version
    
        Bump js-yaml to 4.1.0
    
    commit ff912ae44d2c14b42f8a495990ec61cd30cb4d69
    Author: dappnodedev <diego@dappnode.io>
    Date:   Mon Mar 11 17:58:03 2024 +0100
    
        Bump js-yaml to 4.1.0

---
+++ src/commands/githubActions/bumpUpstream/github/index.ts
@@ -0,0 +1,6 @@
+export * from "./closeOldPrs.js";
+export * from "./getBumpPrBody.js";
+export * from "./getGithubSettings.js";
+export * from "./getUpstreamVersionTag.js";
+export * from "./isBranchNew.js";
+export * from "./isValidRelease.js";
