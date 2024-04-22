69696a53fb9227c5fec17a7e701705ab93af53d0
dappnodedev <diego@dappnode.io>
2024-04-22 13:16:07 +0200

    Add spacing

--- src/commands/githubActions/bumpUpstream/github/getUpstreamVersionTag.ts
+++ src/commands/githubActions/bumpUpstream/github/getUpstreamVersionTag.ts
@@ -4,6 +4,7 @@ import { ComposeVersionsToUpdate } from "../types.js";
  * This function is used to get the upstream version from the versionsToUpdate object
  * It is used only for the legacy format, which includes upstreamVersion, upstreaRepo and upstreamArg fields
  * The value returned by this function is set to the upstreamVersion field in the manifest
+ * 
  * @param versionsToUpdate Object with the versions to update
  * @returns The upstream version
  * @throws Error if there are multiple versions to update (not supported in legacy upstream format)
