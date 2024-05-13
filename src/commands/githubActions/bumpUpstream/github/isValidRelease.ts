9b0654bcc955cda10cc8878734476358b1fbea7c
dappnodedev <diego@dappnode.io>
2024-05-13 11:14:25 +0200

    Allow non-semver releases

--- src/commands/githubActions/bumpUpstream/github/isValidRelease.ts
+++ src/commands/githubActions/bumpUpstream/github/isValidRelease.ts
@@ -4,10 +4,16 @@ export function isValidRelease(version: string): boolean {
   // Nightly builds are not considered valid releases (not taken into account by semver)
   if (version.includes("nightly")) return false;
 
-  if (!semver.valid(version)) return false;
+  if (semver.valid(version)) {
+    const preReleases = semver.prerelease(version);
 
-  const preReleases = semver.prerelease(version);
+    // A version is considered a valid release if it has no pre-release components.
+    return preReleases === null || preReleases.length === 0;
+  }
 
-  // A version is considered a valid release if it has no pre-release components.
-  return preReleases === null || preReleases.length === 0;
+  console.warn(
+    `Upstream version (${version}) does not follow semver. This might be a release candidate or pre-release.`
+  );
+
+  return true;
 }
