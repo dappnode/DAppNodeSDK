8073906810c2831c7778459aa851d5c4da925c1d
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-10-16 12:19:54 +0200

    Read file by chunks to avoid error of maximum size `ERR_FS_FILE_TOO_LARGE` (#450)
    * Read file by chunks to avoid error of maximum size `ERR_FS_FILE_TOO_LARGE`
    
    * use node 20 in gha

--- src/utils/getFileHash.ts
+++ src/utils/getFileHash.ts
@@ -2,21 +2,31 @@ import fs from "fs";
 import crypto from "crypto";
 
 /**
- * Hashes a file's buffer
+ * Hashes a file by reading it in chunks
  *
  * @param path
- * @return file's sha3 hash: 0x36d2fe6d4582e8cc1e5ea4c6c05e44bc94b88f4567edca12ba5fd5745796edef
+ * @return file's sha256 hash
  */
 export function getFileHash(path: string): Promise<string | null> {
-  return fs.promises
-    .readFile(path)
-    .then(data => {
-      const hash = crypto.createHash("sha256");
-      hash.update(data);
-      return hash.digest("hex");
-    })
-    .catch(e => {
-      if (e.code === "ENOENT") return null;
-      else throw e;
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash("sha256");
+    const stream = fs.createReadStream(path);
+
+    stream.on("data", (chunk: Buffer) => {
+      hash.update(chunk); // Should now recognize Buffer correctly
     });
+
+    stream.on("end", () => {
+      const fileHash = hash.digest("hex");
+      resolve(fileHash);
+    });
+
+    stream.on("error", (err: NodeJS.ErrnoException) => {
+      if (err.code === "ENOENT") {
+        resolve(null); // file not found
+      } else {
+        reject(err); // other errors
+      }
+    });
+  });
 }
